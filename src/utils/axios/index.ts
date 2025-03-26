6ad296ace30f68a4617ab44b67fb2defdbfd6098
phucdh410 <phucdh410@gmail.com>
2025-03-26 16:52:04 +0700

    UPDATE: handle date

--- src/utils/axios/index.ts
+++ src/utils/axios/index.ts
@@ -28,4 +28,81 @@ export const setAuthToken = (token?: string) => {
   else delete axiosInstance.defaults.headers.common["Authorization"];
 };
 
-export { axiosInstance };
+// const isValidDateValue = (value) => {
+//   const date = new Date(value);
+//   const isISOString = !isNaN(date.getTime()) && value === date.toISOString();
+
+//   const dayjsValue = dayjs(value);
+//   const isDateFormat = dayjsValue.format("YYYY-MM-DD");
+//   const isDatetimeFormat = dayjsValue.format("YYYY-MM-DD HH:mm:ss");
+//   const isMatchFormat = value === isDateFormat || value === isDatetimeFormat;
+
+//   return (
+//     value instanceof Date || isDayjs(value) || isISOString || isMatchFormat
+//   );
+// };
+
+// const payload = {
+//   id: 1,
+//   _id: "absd22",
+//   name: "Phuc",
+//   age: 22,
+//   birthday: new Date(),
+//   address: "HoChiMinh",
+//   gender: true,
+//   family: {
+//     dad: {
+//       name: "A",
+//       date: "1963-10-26",
+//     },
+//     mom: {
+//       name: "B",
+//       date: "1967-05-17",
+//     },
+//     brother: {
+//       name: "C",
+//       date: new Date().getTime(),
+//     },
+//     other: {
+//       name: "D",
+//       date: dayjs().startOf("month"),
+//     },
+//   },
+//   achivements: [
+//     {
+//       name: "Win 2022",
+//       date: dayjs(),
+//     },
+//     {
+//       name: "Top 1",
+//       date: "2024-01-01 20:20:11",
+//     },
+//     {
+//       name: "Hehe",
+//       date: "2024/10/10",
+//     },
+//   ],
+// };
+
+// const formatDateFields = (data) => {
+//   for (const [key, value] of Object.entries(data)) {
+//     if (isValidDateValue(value)) data[key] = "MODIFIED";
+//     else if (typeof value === "object") {
+//       if (Array.isArray(value)) {
+//         value.forEach((e) => {
+//           if (typeof e === "object" && e !== null) {
+//             formatDateFields(e);
+//           }
+//         });
+//       } else if (value !== null) {
+//         formatDateFields(value);
+//       }
+//     }
+//   }
+// };
+
+// const testResult = { ...payload };
+
+// formatDateFields(testResult);
+
+// console.log(testResult);
