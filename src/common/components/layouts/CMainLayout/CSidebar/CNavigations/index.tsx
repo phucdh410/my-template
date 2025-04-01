0795ca097dc66d9327954f76a6cad967d17998ea
phucdh410 <phucdh410@gmail.com>
2025-04-01 11:58:18 +0700

    UPDATE: UI

--- src/common/components/layouts/CMainLayout/CSidebar/CNavigations/index.tsx
+++ src/common/components/layouts/CMainLayout/CSidebar/CNavigations/index.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useRef } from "react";
+
+import Scrollbar from "smooth-scrollbar";
+
 import { NAVIGATIONS } from "@/constants/navigations";
 
 import { CGroup } from "../CGroup";
 
+import "./styles.scss";
+
 export const CNavigations = () => {
+  const scrollbarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (scrollbarRef.current) {
+      const scrollbar = Scrollbar.init(scrollbarRef.current, {
+        damping: 0.01525, // Adjust smoothness
+        thumbMinSize: 10,
+      });
+
+      return () => {
+        scrollbar.destroy();
+      };
+    }
+  }, []);
+
   return (
-    <nav style={{ paddingInline: "16px", flex: 1 }}>
+    <nav
+      className="c-navigation"
+      ref={scrollbarRef}
+      style={{ paddingInline: "16px", flex: 1 }}
+    >
       <ul style={{ display: "flex", flexDirection: "column" }}>
         {NAVIGATIONS.map((nav, index) => (
           <CGroup key={index} group={nav} />
