281d2aa817372fe25c540bc4bfbf3e1feeb5f24f
phucdh410 <phucdh410@gmail.com>
2025-04-03 15:18:26 +0700

    UPDATE: UI

--- src/common/components/layouts/CMainLayout/CHeader/index.tsx
+++ src/common/components/layouts/CMainLayout/CHeader/index.tsx
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react";
+
 import { AppBar, Stack } from "@mui/material";
+import classNames from "classnames";
 
 import "./styles.scss";
 
 export const CHeader = () => {
+  //#region Data
+  const [outTop, setOutTop] = useState(false);
+  //#endregion
+
+  useEffect(() => {
+    const mainEl = document.getElementById("layout-container");
+    if (!mainEl) return;
+
+    const handleScroll = () => {
+      const { scrollTop } = mainEl;
+      if (scrollTop !== 0) setOutTop(true);
+      else setOutTop(false);
+    };
+
+    mainEl.addEventListener("scroll", handleScroll);
+
+    return () => mainEl.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  //#region Render
   return (
-    <AppBar component="header" position="sticky" id="c-header">
+    <AppBar
+      component="header"
+      position="sticky"
+      id="c-header"
+      className={classNames(outTop && "scroll-out-top")}
+    >
       <Stack
         className="c-header--container"
         direction="row"
@@ -18,4 +46,5 @@ export const CHeader = () => {
       </Stack>
     </AppBar>
   );
+  //#endregion
 };
--- src/common/components/layouts/CMainLayout/index.tsx
+++ src/common/components/layouts/CMainLayout/index.tsx
@@ -1,31 +1,20 @@
-import { useState } from "react";
 import { Outlet } from "react-router";
 
-import { Stack, useScrollTrigger } from "@mui/material";
+import { Stack } from "@mui/material";
 
 import { CHeader } from "./CHeader";
 import { CSidebar } from "./CSidebar";
 
 export const CMainLayout = () => {
-  const [scrollTarget, setScrollTarget] = useState<HTMLElement | null>(null);
-  const trigger = useScrollTrigger({ target: scrollTarget });
-  console.log("🚀 ~ CMainLayout ~ trigger:", trigger);
-
   return (
     <Stack direction="row" height="100vh" width="100vw" overflow="hidden">
       <CSidebar />
-      <Stack
-        ref={(node) => {
-          if (node) {
-            setScrollTarget(node);
-          }
-        }}
-        flex={1}
-        sx={{ overflowY: "auto" }}
-      >
+      <Stack id="layout-container" flex={1} sx={{ overflowY: "auto" }}>
         <CHeader />
 
-        <Outlet />
+        <main>
+          <Outlet />
+        </main>
       </Stack>
     </Stack>
   );
